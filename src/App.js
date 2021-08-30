e99a9c4ba0165d33e79315c2a2a5966e45881f87
rosindubh <p.welsby2@ntlworld.com>
2021-08-30 22:38:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import react, {useState} from "react";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password ) {
+      console.log("Logged in");
+    } else {
+      console.log("Details do not match")
+    }
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+  }
+
+  return (
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome <span>{user.name}</span></h2>
+          <button>Logout</button>
+        </div>
+      ) :  (
+        <LoginForm  Login={Login} error={error}/>
+      )}
+    </div>
+  );
+}
+
+export default App;
